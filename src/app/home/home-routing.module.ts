7e57392840259f363d235db8e7a23a58241c695b
Syam Pradeep <syampradeep2b6@gmail.com>
2019-07-13 10:23:42 +0530

    added jobs configuration components

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 const routes: Routes = [ 
-  { path: '', component: HomeComponent }
-
+  { path: '', component: HomeComponent, 
+  children: [
+    {
+      path: 'jobs', component: JobsComponent
+    },
+    {
+      path: '', redirectTo: 'jobs'
+    },
+    {
+      path: '', component: ConfigurationComponent
+    }
+  ]},
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MatToolbarModule, MatButtonModule, MatTableModule } from '@angular/material';
+import { JobsComponent } from './jobs/jobs.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    JobsComponent,
+    ConfigurationComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTableModule
   ]
 })
 export class HomeModule { }
